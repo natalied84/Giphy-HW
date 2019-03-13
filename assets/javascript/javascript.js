8a559b959d22b1835856003ccf7c31106f2421d7
Natalied84 <n-dreher@hotmail.com>
2019-03-12 20:31:40 -0600

    I lied, I made it give new gifs every additional time you click a button

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,6 @@
 var reactions = ["confused", "shocked", "neat", "shrug", "table flip", "rage quit", "excited", "sad", "crying", "facepalm"];
 var reactionTracker = [];
+var reactionClickArray = [];
 
 function doABarrelRoll() {
     var a = "-webkit-",
@@ -21,38 +22,49 @@ function renderButtons() {
 
 $("header").on("click", ".react-btn", function () {
 
-    var searchTerm = $(this).attr("id");
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=UU8fjeOg71QTR3pqQBPam5po75K6tgIJ&q=" + searchTerm + "&limit=10&offset=0&rating=PG&lang=en"
+    var offset = 0;
+    var reactionCounter = -1;
+    reactionClickArray.push($(this).attr("id"));
 
-    if ($(this).attr("id") === "do a barrel roll") {
-        doABarrelRoll()
-        setTimeout(function () {
-            $("style").remove()
-        }, 4100);
+    for (var i = 0; i < reactionClickArray.length; i++) {
+        
+        if ($(this).attr("id") === reactionClickArray[i]) {
+            reactionCounter++;
+        };
     };
 
+    offset += (reactionCounter * 10);
+    var searchTerm = $(this).attr("id");
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=UU8fjeOg71QTR3pqQBPam5po75K6tgIJ&q=" + searchTerm + "&limit=10&offset=" + offset + "&rating=PG&lang=en"
+    
+    if ($(this).attr("id") === "do a barrel roll") {
+        doABarrelRoll();
+        setTimeout(function () {
+            $("style").remove();
+        }, 4100);
+    };
+    
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(function (response) {
 
-        console.log(response)
+        console.log(response);
         var result = response.data;
 
         for (var i = 0; i < result.length; i++) {
 
             var gifDiv = $("<div class='gifs'>");
             var rated = result[i].rating;
-            var stillGifUrl = result[i].images.fixed_height_still.url
-            var animateGifUrl = result[i].images.fixed_height.url
-            var p = $("<p>").text("Rated: " + rated)
-            var gif = $("<img class='gifImg' src='" + stillGifUrl + "'>")
-
-            gif.attr("data-state", "still")
-            gif.attr("data-animate", animateGifUrl)
-            gif.attr("data-still", stillGifUrl)
-            $(gifDiv).append(p, gif)
-            $("#gifs-holder").prepend(gifDiv)
+            var stillGifUrl = result[i].images.fixed_height_still.url;
+            var animateGifUrl = result[i].images.fixed_height.url;
+            var p = $("<p>").text("Rated: " + rated);
+            var gif = $("<img class='gifImg' src='" + stillGifUrl + "'>");
+            gif.attr("data-state", "still");
+            gif.attr("data-animate", animateGifUrl);
+            gif.attr("data-still", stillGifUrl);
+            $(gifDiv).append(p, gif);
+            $("#gifs-holder").prepend(gifDiv);
         };
     });
 });
